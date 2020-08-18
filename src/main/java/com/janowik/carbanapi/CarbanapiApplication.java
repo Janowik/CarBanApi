330b2bfe5a87dd2871435ab36a4529f634a60a29
janowik <jacek.nowak.jn@gmail.com>
2020-08-18 14:13:34 +0200

    firs commit

---
+++ src/main/java/com/janowik/carbanapi/CarbanapiApplication.java
@@ -0,0 +1,13 @@
+package com.janowik.carbanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbanapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbanapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janowik/carbanapi/CarbanapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janowik.carbanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbanapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
